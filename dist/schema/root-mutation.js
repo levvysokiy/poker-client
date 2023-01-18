1cff5d9f1f5df764a533c2d3c66c2b0a363a35f3
Lev Vysokiy <vysokiylo@silaunion.ru>
2023-01-19 00:23:13 +0500

    add dist to gitignore

--- dist/schema/root-mutation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const graphql_1 = require("graphql");
-const mutation = new graphql_1.GraphQLObjectType({
-    name: 'Mutation',
-    fields: () => ({}),
-});
-exports.default = mutation;
